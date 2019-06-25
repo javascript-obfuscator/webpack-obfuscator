60bed5418074e43c63c35231dfdb498418dccb99
Maurice Doepke <mauricepdoepke@gmail.com>
2019-06-25 19:35:09 +0200

    fix git command

--- push-build.sh
+++ push-build.sh
@@ -9,4 +9,4 @@ git commit -m "Travis Build: $TRAVIS_BUILD_NUMBER" -m "[skip ci]"
 git remote rm origin
 # Add new "origin" with access token in the git URL for authentication
 git remote add origin https://mauricedoepke:${GH_TOKEN}@github.com/mauricedoepke/webpack-obfuscator.git > /dev/null 2>&1
-git push origin master --quiet
+git push origin master
