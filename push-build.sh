44bb2483040760aea7c251f578a519b91825a5d9
Maurice Doepke <mauricepdoepke@gmail.com>
2019-06-25 19:28:48 +0200

    fix git command

--- push-build.sh
+++ push-build.sh
@@ -3,9 +3,10 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-sed -i '/dist/d' .gitignore
 git add dist/*
-git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git commit -m "Travis Build: $TRAVIS_BUILD_NUMBER" -m "[skip ci]"
 
+git remote rm origin
+# Add new "origin" with access token in the git URL for authentication
 git remote add origin https://mauricedoepke:${GH_TOKEN}@github.com/mauricedoepke/webpack-obfuscator.git > /dev/null 2>&1
-git push --quiet --set-upstream origin master 
+git push origin master --quiet
