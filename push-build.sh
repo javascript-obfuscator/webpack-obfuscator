1b30126d3819d50e491339aba80ee9a7a4e039d4
Maurice Doepke <mauricepdoepke@gmail.com>
2019-06-25 19:14:30 +0200

    fix git command

--- push-build.sh
+++ push-build.sh
@@ -7,5 +7,5 @@ sed -i '/dist/d' .gitignore
 git add dist/*
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git remote add origin https://${GH_TOKEN}@github.com/mauricedoepke/webpack-obfuscator.git > /dev/null 2>&1
+git remote add origin https://mauricedoepke:${GH_TOKEN}@github.com/mauricedoepke/webpack-obfuscator.git > /dev/null 2>&1
 git push --quiet --set-upstream origin master 
