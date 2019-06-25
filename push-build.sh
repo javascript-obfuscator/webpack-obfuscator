912c011ea3705b8ae00227744718dbe5cf51cf16
Maurice Doepke <mauricepdoepke@gmail.com>
2019-06-25 17:56:12 +0200

    auto build test

---
+++ push-build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git checkout -b master
+git add . *.html
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+
+git remote add origin https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+git push --quiet --set-upstream origin master 
